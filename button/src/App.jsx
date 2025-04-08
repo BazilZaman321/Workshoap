d19ef88f869d16b91ec885abda19012f6a63d374
bazil <zamanbazil123@gmail.com>
2025-04-08 08:03:02 +0530

    update

--- button/src/App.jsx
+++ button/src/App.jsx
@@ -14,19 +14,19 @@ function App() {
     <div className="p-4 text-center">
       <button
         onClick={() => handleClick('Button 1')}
-        className="px-4 py-2 bg-blue-500 text-white rounded m-2"
+        className="px-4 py-2 border border-gray-400 rounded m-2"
       >
         Button 1
       </button>
       <button
         onClick={() => handleClick('Button 2')}
-        className="px-4 py-2 bg-green-500 text-white rounded m-2"
+        className="px-4 py-2 border border-gray-400 rounded m-2"
       >
         Button 2
       </button>
       <button
         onClick={() => handleClick('Button 3')}
-        className="px-4 py-2 bg-red-500 text-white rounded m-2"
+        className="px-4 py-2 border border-gray-400 rounded m-2"
       >
         Button 3
       </button>
@@ -39,3 +39,55 @@ function App() {
 }
 
 export default App
+
+
+
+// import React from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { setClickedText } from './store/buttonSlice'
+
+// function App() {
+//   const dispatch = useDispatch()
+//   const clickedColor = useSelector((state) => state.button.clickedText)
+
+//   const handleClick = (color) => {
+//     dispatch(setClickedText(color))
+//   }
+
+//   return (
+//     <div className="p-4 text-center">
+//       <button
+//         onClick={() => handleClick('red')}
+//         className="px-4 py-2 border border-gray-400 rounded m-2"
+//       >
+//         Red
+//       </button>
+//       <button
+//         onClick={() => handleClick('blue')}
+//         className="px-4 py-2 border border-gray-400 rounded m-2"
+//       >
+//         Blue
+//       </button>
+//       <button
+//         onClick={() => handleClick('green')}
+//         className="px-4 py-2 border border-gray-400 rounded m-2"
+//       >
+//         Green
+//       </button>
+
+//       {clickedColor && (
+//         <div
+//           className="mx-auto mt-6"
+//           style={{
+//             backgroundColor: clickedColor,
+//             width: '100px',
+//             height: '100px',
+//             borderRadius: '8px',
+//           }}
+//         ></div>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default App
