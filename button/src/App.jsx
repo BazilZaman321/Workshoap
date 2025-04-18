17d0de21682c81818eb19e0963124d6529845f08
bazil <zamanbazil123@gmail.com>
2025-04-18 16:16:06 +0530

    update

--- button/src/App.jsx
+++ button/src/App.jsx
@@ -1,93 +1,13 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { setClickedText } from './store/buttonSlice'
+import Register from './Component/Register'
 
 function App() {
-  const dispatch = useDispatch()
-  const clickedText = useSelector((state) => state.button.clickedText)
-
-  const handleClick = (text) => {
-    dispatch(setClickedText(text))
-  }
-
   return (
-    <div className="p-4 text-center">
-      <button
-        onClick={() => handleClick('Button 1')}
-        className="px-4 py-2 border border-gray-400 rounded m-2"
-      >
-        Button 1
-      </button>
-      <button
-        onClick={() => handleClick('Button 2')}
-        className="px-4 py-2 border border-gray-400 rounded m-2"
-      >
-        Button 2
-      </button>
-      <button
-        onClick={() => handleClick('Button 3')}
-        className="px-4 py-2 border border-gray-400 rounded m-2"
-      >
-        Button 3
-      </button>
-
-      {clickedText && (
-        <p className="mt-4 text-xl font-semibold">You clicked: {clickedText}</p>
-      )}
+    <div>
+      <h2>Register</h2>
+      <Register/>
     </div>
   )
 }
 
 export default App
-
-
-
-// import React from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { setClickedText } from './store/buttonSlice'
-
-// function App() {
-//   const dispatch = useDispatch()
-//   const clickedColor = useSelector((state) => state.button.clickedText)
-
-//   const handleClick = (color) => {
-//     dispatch(setClickedText(color))
-//   }
-
-//   return (
-//     <div className="p-4 text-center">
-//       <button
-//         onClick={() => handleClick('red')}
-//         className="px-4 py-2 border border-gray-400 rounded m-2"
-//       >
-//         Red
-//       </button>
-//       <button
-//         onClick={() => handleClick('blue')}
-//         className="px-4 py-2 border border-gray-400 rounded m-2"
-//       >
-//         Blue
-//       </button>
-//       <button
-//         onClick={() => handleClick('green')}
-//         className="px-4 py-2 border border-gray-400 rounded m-2"
-//       >
-//         Green
-//       </button>
-
-//       {clickedColor && (
-//         <div
-//           className="mx-auto mt-6"
-//           style={{
-//             backgroundColor: clickedColor,
-//             width: '100px',
-//             height: '100px',
-//             borderRadius: '8px',
-//           }}
-//         ></div>
-//       )}
-//     </div>
-//   )
-// }
-
-// export default App
---
+++ button/src/Component/Register.jsx
@@ -0,0 +1,92 @@
+// import React,{useState} from 'react'
+
+
+// const Register=()=>{
+//     const [password ,setPassword]=  useState(" ");
+
+//     const handleRegister =(e)=>{
+//         e.preventDefault();
+//         setPassword(" ");
+//     }
+//     return(
+//         <form onSubmit={handleRegister}>
+//             <input  
+//             type='password'
+//             placeholder='Enter password'
+//             value={password}
+//             onChange={(e)=>
+//                 setPassword(e.target.value)
+//             }required
+//             />
+
+//         </form>
+//     );
+// };
+
+
+// export default Register
+
+
+
+import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+
+const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 border border-gray-200 rounded-lg shadow-md">
+      <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
+
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {() => (
+          <Form className="space-y-5">
+            {/* Email Field */}
+            <div>
+              <label className="block text-sm font-medium mb-1">Email</label>
+              <Field
+                type="email"
+                name="email"
+                className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Password Field with Eye Toggle */}
+            <div>
+              <label className="block text-sm font-medium mb-1">Password</label>
+              <div className="relative">
+                <Field
+                  type={showPassword ? 'text' : 'password'}
+                  name="password"
+                  className="w-full px-4 py-2 border border-gray-300 rounded pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <span
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-600"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </span>
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
+            >
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginForm;
