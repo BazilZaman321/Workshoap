282eff24f1906d8149294b829cedb29f717382a0
bazil <zamanbazil123@gmail.com>
2025-04-26 09:50:28 +0530

    update

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,70 @@
-let name = "Bazil";
-console.log("Hello, " + name); 
+// let name = "Bazil";
+// console.log("Hello, " + name); 
 
 
-alert("Hello, Bazil!");
+
+// // call stack
+
+
+// function man () {
+//     console.log("hello mAN")
+// }
+// function good(params) {
+//     man()
+//     console.log("good boy")
+// }
+// good()
+
+// stack underflow
+
+
+
+// function one(params) {
+//     one()
+// }
+// one()
+
+// stack underflow
+
+// let stack =[];
+// let remove =stack.pop();
+// console.log("removed")
+
+
+                                    //  global scope
+
+// let two="bazil";
+// function three(params) {
+//     console.log(two);
+// }
+// three()
+
+                                //    functional scope
+
+// function four(params) {
+//     let five="zaman";
+//     console.log(five);
+// }
+// four()
+// console.log(five)
+
+                                     // blcok scope
+
+// {
+//     let six ="biriyani";
+//     console.log(six);
+//
+// console.log(six);
+
+
+                            // lexical scope
+
+// function outer(params) {
+//     let city="mlp";
+//     function inner(params) {
+//         console.log(city);
+//     }
+//     inner()
+// } 
+// outer()
+
