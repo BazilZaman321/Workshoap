3290f3ff0d0f4b390ef2d122bd0f38af232643f8
bazil <zamanbazil123@gmail.com>
2025-04-28 08:32:22 +0530

    update

--- javascript/index.js
+++ javascript/index.js
@@ -133,5 +133,114 @@
 // },1000)
 
 
-////  promise
+                                                ////  promise
 
+// let food=new Promise((resolve,reject)=>{
+//     let hunger=false;
+//     if (hunger){
+//         resolve("done");
+//     }else{
+//         reject("not  done")
+//     }
+// });
+// food
+// .then((msg)=>{
+//     console.log(msg);
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+
+                                    //// closure
+
+// function outer(params) {
+//     let name ="i am bazil";
+//     function inner(params) {
+//         console.log(name);
+//     } return inner()
+// }
+// const good= outer();
+// good();
+
+
+
+                    //// iife(immeadietly invoke function expression )
+
+// (function good() {
+//     console.log("hello");
+    
+// })();
+
+                        //// constroctor function 
+
+// function bad(name , age) {
+//     this.name=name;
+//     this.age =age;   
+// }
+// const one= new bad("bazil",2000)
+
+// console.log(one.age);
+
+
+                            //// generator functionn 
+
+// function* mad(params) {
+//     yield "one";
+//     yield "two";
+//     yield "three";
+// };
+// const mood= mad();
+// console.log(mood.next().value);
+// console.log(mood.next().value);
+// console.log(mood.next().value); 
+
+
+
+                                            //// async and await
+
+
+
+
+// async function ten(params) {
+//     let res=await fetch("https://jsonplaceholder.typicode.com/users/1");
+//     let data=await res.json();
+//     console.log(data);
+
+// }
+// ten()
+
+
+
+                                    //// event loop
+
+// console.log("one");
+// setTimeout(()=>{
+//     console.log("two");
+    
+// },2000);
+// console.log("three");
+
+
+
+                        //// optional chaining
+
+// const man={
+//     user:"bazil",
+// };
+
+// console.log(man.user)
+
+                                //// curining
+
+// function add(a) {
+//     return function(b){
+//         return function (c){
+//             return a+b+c;
+//         }
+//     }
+    
+// }
+// console.log(add(1)(2)(3));
+
+//// 
